99a96927e3c9841b82ee62ff27653a455fb5d5fe
Hariraagavanthar C R <ragavantar@gmail.com>
2019-06-06 13:00:00 +0530

    added nav component

---
+++ src/component/Nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.scss";
+const Nav = () => {
+  return (
+    <nav>
+      <h1>title</h1>
+      <ul>
+        <li>About</li>
+        <li>Photography</li>
+        <li>Service</li>
+        <li>Buy</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
