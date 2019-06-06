0fcd112de7f71c3e83d51a5baeadab0a5fb804bf
Hariraagavanthar C R <ragavantar@gmail.com>
2019-06-06 13:30:44 +0530

    added about component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from "react";
 import "./App.css";
 
 import Nav from "./component/Nav/Nav";
-
+import About from "./component/About/About";
 function App() {
   return (
     <React.Fragment>
       <Nav />
+      <div className="content">
+        <About />
+      </div>
     </React.Fragment>
   );
 }
---
+++ src/component/About/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./About.scss";
+const About = () => {
+  return (
+    <article className="about">
+      <figure>
+        <img
+          src="https://photos.smugmug.com/Other/Customization-Files/n-2qK2H/i-7GVShbV/0/c7d0994c/X4/i-7GVShbV-X4.jpg"
+          alt="photo"
+        />
+      </figure>
+      <div>
+        <h1>ABouT</h1>
+        <p> paraaha[araaga paaaah</p>
+        <p> paraaha[araaga paaaah</p>
+        <p> paraaha[araaga paaaah</p>
+        <p> paraaha[araaga paaaah</p>
+      </div>
+    </article>
+  );
+};
+
+export default About;
