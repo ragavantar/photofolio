1ffe46a9578c9da9e749c2c9fe962c332a02990c
unknown <ragavantar@gmail.com>
2019-06-08 16:45:12 +0530

    added routing

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Nav.scss";
 const Nav = () => {
   return (
     <nav>
       <h1>title</h1>
       <ul>
-        <li>About</li>
-        <li>Photography</li>
-        <li>Contact</li>
-        <li>Shop</li>
+        <li><Link to="/">About</Link></li>
+        <li><Link to="/photography">Photography</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/shop">Shop</Link></li>
       </ul>
     </nav>
   );
