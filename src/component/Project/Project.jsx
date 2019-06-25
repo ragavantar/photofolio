976f200ce6288cfbc7f46b2e2d777d1fecef1bb4
Hariraagavanthar C R <ragavantar@gmail.com>
2019-06-25 14:51:39 +0530

    created project module

---
+++ src/component/Project/Project.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://vikashvvn2.000webhostapp.com/portfolio/api/getProject.php")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // this.setState({
+          //   isLoaded: true,
+          //   items: result.items
+          // });
+          console.log(result)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+ 
+export default Project;
