40865c14ee681245b18f5391bdea5b74fbb2219c
Hariraagavanthar C R <hari.raghavandhar@myntra.com>
2020-01-28 15:55:00 +0530

    about content updated

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -12,8 +12,11 @@ const About = () => {
       </figure>
       <div>
         <h1>ABOUT</h1>
-        <p>Wellcome to my perspective of world. Finished my Diploma in Fashion Photography in 2016 at JD INSTITUTE OF FASHION TECHNOLOGY , BANGALORE. Experience of 3+years in portraits,weddings, fashion, corporate events and so on . Based in Bangalore,but love to travel and work .Having descent knowledge in Photoshop and Lightroom to give my final enhance to the image.Storytelling is one of the strongest mode of way representing a picture which I'm always drawn too. I Really take pride in capturing the true emotions and personality of people and subjects I photograph .I appreciate the people I meet n portray them. These story telling pictures makes them memorable and iconic it's my strongest asset. 
-        I specialize in portraits , weddings , Fashion , Events and travel portraits. Look forward to meeting you and work with you .</p>
+        {/* <p>Wellcome to my perspective of world. Finished my Diploma in Fashion Photography in 2016 at JD INSTITUTE OF FASHION TECHNOLOGY , BANGALORE. Experience of 3+years in portraits,weddings, fashion, corporate events and so on . Based in Bangalore,but love to travel and work .Having descent knowledge in Photoshop and Lightroom to give my final enhance to the image.Storytelling is one of the strongest mode of way representing a picture which I'm always drawn too. I Really take pride in capturing the true emotions and personality of people and subjects I photograph .I appreciate the people I meet n portray them. These story telling pictures makes them memorable and iconic it's my strongest asset. 
+        I specialize in portraits , weddings , Fashion , Events and travel portraits. Look forward to meeting you and work with you .</p> */}
+        <p>
+        Welcome to my perspective of world. Finished my Diploma in Fashion Photography in 2016 at JD INSTITUTE OF FASHION TECHNOLOGY , BANGALORE. Experience of 3+years in portraits,weddings, fashion, corporate events and so on. Based in Bangalore, but love to travel and work. Having descent knowledge in Photoshop and Lightroom to give my final enhance to the image. Storytelling is one of the strongest mode of representing a picture which I have always drawn too. I really take pride in capturing the true emotions and personality of people and subjects, I photograph. I appreciate the people who I meet and portray them. These story telling pictures make them memorable and iconic which is the strongest asset for me. I specialize in portraits, weddings,  fashion, events and travel portraits. Looking forward to meet and work with you.
+        </p>
         <p>
         Thank you ..
         </p>
